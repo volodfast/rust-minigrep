386ac89e581d37f5f2f83c5173dea0a81009fec9
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-17 00:07:30 +0200

    Add custom error message if not enough argument specified

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -20,9 +24,14 @@ struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Self {
+    pub fn new(args: &[String]) -> Result<Self, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
-        Self { query, filename }
+
+        Ok(Self { query, filename })
     }
 }
