dacf3dba6139f71305b97e60ea9ab4fde8c635c9
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-11-16 23:58:25 +0200

    Move parsing config logic into Config::new method

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -19,9 +19,10 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
+impl Config {
+    pub fn new(args: &[String]) -> Self {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Self { query, filename }
+    }
 }
